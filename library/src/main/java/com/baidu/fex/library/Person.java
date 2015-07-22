cc0b53ed3464c11f54e5f3f4d88c431183fff36c
zhangzimeng01@baidu.com <zhangzimeng01>
2015-07-22 21:06:16 +0800

    init

---
+++ library/src/main/java/com/baidu/fex/library/Person.java
@@ -0,0 +1,14 @@
+package com.baidu.fex.library;
+
+public class Person {
+    private String who = "zimeng";
+
+    public String getWho() {
+        return who;
+    }
+
+    public String getName(){
+        return "zhang";
+    }
+
+}
